df7ec49629be807258f9a733f9df51a8b848564b
Thiago-PT <166539095+Thiago-PT@users.noreply.github.com>
2024-11-03 17:30:45 -0500

    Create registro.php

---
+++ registro.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json');
+$conexion = new mysqli('b4qhbwwqys2nhher1vul-mysql.services.clever-cloud.com', 'upvge9afjesbmmgv', 'BS2bxJNACO1XYEmWBqA0 ', 'upvge9afjesbmmgv');
+
+if ($conexion->connect_error) {
+    echo json_encode(["status" => "error", "message" => "Conexión fallida"]);
+    exit();
+}
+
+$correo = $_POST['correo'];
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO usuario (correo, password) VALUES (?, ?)";
+$stmt = $conexion->prepare($sql);
+$stmt->bind_param("ss", $correo, $password);
+
+if ($stmt->execute()) {
+    echo json_encode(["status" => "success"]);
+} else {
+    echo json_encode(["status" => "error", "message" => "No se pudo crear el usuario"]);
+}
+
+$stmt->close();
+$conexion->close();
+?>
