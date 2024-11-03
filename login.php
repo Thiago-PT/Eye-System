45efd48640201b914772f9e6663d3c7dda3ef078
Thiago-PT <166539095+Thiago-PT@users.noreply.github.com>
2024-11-03 17:29:11 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-$conexion = new mysqli('sql208.infinityfree.com', 'if0_37334421', 'iLoveWindows ', 'if0_37334421_asistencia');
+$conexion = new mysqli('b4qhbwwqys2nhher1vul-mysql.services.clever-cloud.com', 'upvge9afjesbmmgv', 'BS2bxJNACO1XYEmWBqA0 ', 'b4qhbwwqys2nhher1vul');
 
 if ($conexion->connect_error) {
     echo json_encode(["status" => "error", "message" => "Conexión fallida"]);
