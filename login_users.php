d65a65014e8bb8a151425bbf36c481f5ab50e910
Thiago-PT <166539095+Thiago-PT@users.noreply.github.com>
2024-11-04 10:57:54 -0500

    Update login_users.php

--- login_users.php
+++ login_users.php
@@ -1,28 +1,35 @@
 <?php
 header('Content-Type: application/json');
+
+// Conexión a la base de datos
 $conexion = new mysqli('b4qhbwwqys2nhher1vul-mysql.services.clever-cloud.com', 'upvge9afjesbmmgv', 'BS2bxJNACO1XYEmWBqA0', 'b4qhbwwqys2nhher1vul');
 
+// Verificar la conexión
 if ($conexion->connect_error) {
     echo json_encode(["status" => "error", "message" => "Conexión fallida"]);
     exit();
 }
 
+// Recibir datos del formulario
 $email = $_POST['email'];
 $contraseña = $_POST['contraseña'];
 
+// Preparar la consulta SQL
 $sql = "SELECT contraseña FROM usuarios WHERE email = ?";
 $stmt = $conexion->prepare($sql);
-$stmt->bind_param("s", $correo);
+$stmt->bind_param("s", $email);
 $stmt->execute();
 $stmt->bind_result($hashed_contraseña);
 $stmt->fetch();
 
+// Verificar la contraseña
 if (password_verify($contraseña, $hashed_contraseña)) {
     echo json_encode(["status" => "success"]);
 } else {
     echo json_encode(["status" => "error", "message" => "Correo o contraseña incorrectos"]);
 }
 
+// Cerrar la declaración y la conexión
 $stmt->close();
 $conexion->close();
 ?>
